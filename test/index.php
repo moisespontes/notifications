cd68afc4397e4f4e5bf10627e3389f2e35f29933
Moises Pontes <sesiom_assis@hotmail.com>
2022-04-05 10:30:16 -0300

    Modificação do arquivo index da pasta de test

--- test/index.php
+++ test/index.php
@@ -1,12 +1,10 @@
 <?php
 
-require __DIR__ . '/../lib_ext/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 use Notifications\Email;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\PHPMailer;
-
-$body = "<p>verificando meu primeiro email</p>";
 
 $notification = new Email();
 $notification->sendMail();
